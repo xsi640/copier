d3217cbfd0c9e4627184154fce787bc01bdfe8fc
xsi640 <xsi64@126.com>
2017-05-09 16:48:14 +0800

    修改日志显示方式

--- src/renderer/index.js
+++ src/renderer/index.js
@@ -1,13 +1,20 @@
 import React, {Component} from 'react'
 import {createStore, applyMiddleware} from 'redux';
 import {Provider} from 'react-redux'
-import logger from 'redux-logger'
 import ReactDOM from 'react-dom'
 import thunk from 'redux-thunk'
 import Page from './containers/page'
 import {reducer} from './reducers/reducers'
 
-const store = createStore(reducer, applyMiddleware(thunk, logger));
+const middlewares = [];
+
+if (process.env.NODE_ENV === `development`) {
+    const { logger } = require(`redux-logger`);
+    middlewares.push(logger);
+}
+middlewares.push(thunk)
+
+const store = createStore(reducer, applyMiddleware(...middlewares));
 
 class App extends Component {
     render() {
